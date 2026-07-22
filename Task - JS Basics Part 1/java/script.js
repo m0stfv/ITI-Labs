8e0f4df2bf80e7ddc6cac047fdb45f29fb445834
Mostafa Samir <m.samir22605748@gmail.com>
2026-07-22 04:54:55 +0300

    Add files via upload

---
+++ Task - JS Basics Part 1/java/script.js
@@ -0,0 +1,10 @@
+// Object
+let student = {
+    name: "Mostafa",
+    age: 23,
+    track: "Web Development",
+    isGraduated: false,
+};
+
+console.log("Student name:", student.name);
+console.log("Student track:", student.track);
